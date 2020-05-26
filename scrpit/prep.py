3a5ed92a654206de350f9d14cb6d1cc5c163500f
bluan2019 <luanboheng@qq.com>
2020-05-26 09:41:52 +0800

    update preprocessing; rmove dulicate, add multiprocess

--- scrpit/prep.py
+++ scrpit/prep.py
@@ -1,38 +1,69 @@
 import jieba
 from tqdm import tqdm
 import random
+import logging
+jieba.setLogLevel(logging.INFO)
+
+from multiprocessing import Pool
+
+def cut_list(data, batch_size):
+    return [data[x:x+batch_size] for x in range(0, len(data), batch_size)]
+
+def parallel(func: callable, data:list, num_worker:int=3, debug=False) -> callable:
+    if num_worker == 1:
+        return func(data)
+
+    batch_size = len(data)//num_worker + 1
+    with Pool(num_worker) as p:
+        if debug: print('parallel debug M:', cut_list(data, batch_size))
+        res = p.map(func, cut_list(data, batch_size))
+        if debug: print('parallel debug R:', res)
+    result = []
+    for e in res: result.extend(e)
+    return result
+
 
 if __name__ == '__main__':
-    path_en = '/data/translate/hb_trans/all.en'
-    path_zh = '/data/translate/hb_trans/all.zh'
-
-    #data_en = open(path_en, 'r').read().split('\n')[:1000]
-    #data_zh = open(path_zh, 'r').read().split('\n')[:1000]
+    path_en  = '/data/lbh/translate/wmt/en-zh/UNv1.0.en-zh.en'
+    path_zh  = '/data/lbh/translate/wmt/en-zh/UNv1.0.en-zh.zh'
+    path_out = "data/"
     
-    data_en = open(path_en, 'r').read().split('\n')
-    data_zh = open(path_zh, 'r').read().split('\n')
+    data_pair = list(zip(open(path_en, 'r').read().split('\n'),
+                         open(path_zh, 'r').read().split('\n')))
+    print(f'total len: {len(data_pair)}')
+    data_pair = set(data_pair)
+    print(f'remove duplicate: {len(data_pair)}')
 
-    data_en = [[e.strip() for e in jieba.lcut(line) if e.strip()] for line in tqdm(data_en, 'load en')]
-    data_zh = [[e.strip() for e in jieba.lcut(line) if e.strip()] for line in tqdm(data_zh, 'load zh')]
+    data_en, data_zh = zip(*data_pair)
+    del data_pair
+   
+    def cut_sent(data):
+        return [[e.strip() for e in jieba.lcut(line) if e.strip()] for line in tqdm(data)]
+    
+    data_en = parallel(cut_sent, data_en, num_worker=40)
+    data_zh = parallel(cut_sent, data_zh, num_worker=40)
 
     data = [(' '.join(en), ' '.join(zh)) for en, zh in zip(data_en, data_zh) \
             if len(en) * len(zh) and 1/1.3 < len(en)/ len(zh) < 1.3]
     random.shuffle(data)
+    print(f'remove 1.3: {len(data)}')
 
     start_point = [int(e*len(data)) for e in [0, 0.05, 0.10]] + [None]
     datasets = [[], [], []]
     for i, dataset in enumerate(datasets):
         dataset.extend(data[start_point[i]:start_point[i+1]])
+
+    print(f'dataset len: {[len(e) for e in datasets]}')
     
     outf = {}
     for k in ['dev', 'test', 'train']:
-        for type in ['src', 'tgt']:
+        for type in ['en', 'zh']:
             name = f'{type}-{k}.txt'
-            outf[name] = open(name, 'w')
+            outf[name] = open(f'{path_out}/{name}', 'w')
 
-    for k, v in zip(['dev', 'test', 'train'], datasets):
+    for k, v in zip(['dev', 'val', 'train'], datasets):
         for en, zh in tqdm(v, f'save {k}'):
-            print(en, file=outf[f'src-{k}.txt'])
-            print(zh, file=outf[f'tgt-{k}.txt'])
+            print(en, file=outf[f'en-{k}.txt'])
+            print(zh, file=outf[f'zh-{k}.txt'])
     
 
--- scrpit/prep.sh
+++ scrpit/prep.sh
@@ -1,18 +1,19 @@
-#python tools/learn_bpe.py --symbols 35000 --input data/src-train.txt --output data/src.bpe
-#python tools/learn_bpe.py --symbols 35000 --input data/tgt-train.txt --output data/tgt.bpe
-#
+#python tools/learn_bpe.py --symbols 35000 --input data/en-train.txt --output data/en.bpe
+#python tools/learn_bpe.py --symbols 35000 --input data/zh-train.txt --output data/zh.bpe
 #echo "finish bpe"
 #
-#for TYPE in src tgt;
+#for TYPE in en zh;
 #do
 #	for DATA in dev val train;
 #	do
 #		echo apply bpe on data/$TYPE-$DATA.txt
-#		python tools/apply_bpe.py --code data/$TYPE.bpe --input data/$TYPE-$DATA.txt --output data/$TYPE-$DATA-bpe.txt
+#		python tools/apply_bpe.py --code data/$TYPE.bpe --input data/$TYPE-$DATA.txt --output data/$TYPE-$DATA-bpe.txt 
 #	done
 #done
 
+
 python preprocess.py \
+:x
 	-train_src data/zh-train-bpe.txt -train_tgt data/en-train-bpe.txt \
 	-valid_src data/zh-val-bpe.txt -valid_tgt data/en-val-bpe.txt \
 	-save_data data/zh2en -overwrite \
